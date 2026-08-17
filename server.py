b0f4c5fb0e904eda0b98ca0e27a408f8793fdee7
TanuGupta2108 <guptatanu090@gmail.com>
2026-08-17 16:34:35 -0400

    Deploy emotion detector using Flask

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+
+@app.route("/emotionDetector", methods=["GET"])
+def emotion_detection():
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    result = emotion_detector(text_to_analyze)
+
+    return (
+        "For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
